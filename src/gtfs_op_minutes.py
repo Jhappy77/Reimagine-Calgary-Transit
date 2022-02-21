74cc97e5a134d9c79f9a27ee1a789f5d2fdc6efa
Jhappy77 <joel.happ@shaw.ca>
2022-02-21 00:37:17 -0700

    Do analysis for Jan 24 2020

--- src/gtfs_op_minutes.py
+++ src/gtfs_op_minutes.py
@@ -1,10 +1,10 @@
-from operator import index
 import pandas as pd
 from datetime import datetime, timedelta
 
-dataset = "calgary_transit_nov_2019"
+# dataset = "calgary_transit_nov_2019"
+dataset = "calgary_transit_jan_24_2020"
 directory = f'src/datasets/{dataset}'
-
+output_dir = f'src/output/{dataset}'
 
 
 FMT = '%H:%M:%S'
@@ -44,16 +44,16 @@ def get_trip_times_new(directory: str)-> pd.DataFrame:
         row_df = pd.DataFrame([trip_row], index=[trip_id], columns=trip_time_columns)
         trip_times = pd.concat([trip_times, row_df], ignore_index=True)
 
-    trip_times.to_csv(f'{directory}/trip_times.txt', index=False)
+    trip_times.to_csv(f'{output_dir}/trip_times.txt', index=False)
 
     return trip_times
 
 # For trip times which have already been generated into directory folder
-def get_trip_times_old(directory: str)->pd.DataFrame:
-    return pd.read_csv(f'{directory}/trip_times.txt')
+def get_trip_times_old(output_dir: str)->pd.DataFrame:
+    return pd.read_csv(f'{output_dir}/trip_times.txt')
 
 # trip_times = get_trip_times_new(directory)
-trip_times = get_trip_times_old(directory)
+trip_times = get_trip_times_old(output_dir)
 # print(new_df.iloc[0])
 # print("generated:")
 # print(gen_df.iloc[0])
@@ -61,10 +61,16 @@ trip_times = get_trip_times_old(directory)
 trips = pd.read_csv(f'{directory}/trips.txt')
 trip_times_with_trip_info = trip_times.merge(trips, on='trip_id')
 trip_times_with_trip_info.sort_values(by=['route_id', 'start_time'], inplace=True)
-regular_weekday_service_id ='2019SE-1BUSWK-Weekday-28'
-trip_times_with_trip_info.drop(trip_times_with_trip_info[trip_times_with_trip_info['service_id'] != regular_weekday_service_id].index, inplace=True)
+#regular_weekday_service_ids = ['2019SE-1BUSWK-Weekday-28']
+regular_weekday_service_id = '2019DE-1BUSWK-Weekday-01'
+weekday_except_friday_service_id = '2019DE-1BUSWK-Weekday-01-1111000'
+indexes_matching_service_id = trip_times_with_trip_info[trip_times_with_trip_info['service_id'] == regular_weekday_service_id].index
+indexes_matching_weekday_sans_friday = trip_times_with_trip_info[trip_times_with_trip_info['service_id'] == weekday_except_friday_service_id].index
+matching_indexes = indexes_matching_service_id.union(indexes_matching_weekday_sans_friday)
+indexes_that_dont_match = trip_times_with_trip_info.index.difference(matching_indexes)
+trip_times_with_trip_info.drop(indexes_that_dont_match, inplace=True)
 
-trip_times_with_trip_info.to_csv(f'{directory}/trip_times_with_trip_info.txt', index=False)
+trip_times_with_trip_info.to_csv(f'{output_dir}/trip_times_with_trip_info.txt', index=False)
 
 
 grouped_by_route_id = trip_times_with_trip_info.groupby('route_id')
@@ -76,6 +82,6 @@ route_op_minutes_w_route_info.to_csv(f'{directory}/route_operating_minutes.txt')
 route_op_minutes_w_route_info['route_short_name'] = pd.to_numeric(route_op_minutes_w_route_info['route_short_name'])
 dropped_above500 = route_op_minutes_w_route_info.drop(route_op_minutes_w_route_info[route_op_minutes_w_route_info['route_short_name'] >= 500].index)
 sorted_route_op_mins = dropped_above500.sort_values(by='route_short_name')
-sorted_route_op_mins.to_csv(f'{directory}/route_operating_minutes.txt', index=False)
+sorted_route_op_mins.to_csv(f'{output_dir}/route_operating_minutes.txt', index=False)
 sum = sorted_route_op_mins['operating mins'].sum()
 print(f'Total operating minutes: {sum}')
